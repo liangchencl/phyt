3ceff233149a2a142bad31b27b21bb2f1ea829fa
凉城 <627150799@qq.com>
2016-11-08 14:12:05 +0800

    01

---
+++ pages/more/more.js
@@ -0,0 +1,20 @@
+var app =getApp()
+Page({
+  data:{
+    "text":"hello"
+  },
+    change:function(e){
+        var type = [
+            "wechat","call_center"
+        ];
+        
+        var id = e.currentTarget.id , data = {};
+        console.log(id);
+        for(var i=0,len=type.length;i<len;i++){
+            data[type[i]+"Show"] = false;
+        }
+        data[id + 'Show'] = !this.data[id + 'Show'];
+        this.setData(data);
+        
+    }
+})
