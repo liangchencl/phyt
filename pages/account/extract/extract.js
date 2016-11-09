c490282b797532aa3208574c06fd4d55e4088798
凉城 <627150799@qq.com>
2016-11-09 14:25:26 +0800

    03

--- pages/account/extract/extract.js
+++ pages/account/extract/extract.js
@@ -2,5 +2,19 @@ var app =getApp()
 Page({
   data:{
     "text":"提现"
+  },
+  change:function(){
+    wx.showModal({
+      title: '提示',
+      content: '提现申请成功，请等待复核！',
+      success: function(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+          wx.navigateTo({
+            url: '../account'
+          })
+        }
+      }
+    })
   }
 })
--- pages/tz/tz.js
+++ pages/tz/tz.js
@@ -1,6 +1,25 @@
 var app =getApp()
 Page({
   data:{
-    "text":"投资0"
+    "text":"投资0",
+    tzList:[]
+  },
+  onLoad:function(e){
+    var that = this;
+    //   console.log(that);
+      wx.request({
+      url: 'https://www.phyt88.com/v2/project/obtain_small_section_list.jso?pageSize=5&pageIndex=1',
+      data:"",
+      method:"POST",
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res);
+        that.setData({
+            tzList:res.data.rows,
+          })
+      }
+    })
   }
 })
