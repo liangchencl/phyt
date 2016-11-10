9e9373eff41495857e268302561c9418f4521160
凉城 <627150799@qq.com>
2016-11-10 16:37:51 +0800

    04

--- pages/account/userShare/userSharerule/userSharerule.js
+++ pages/account/userShare/userSharerule/userSharerule.js
@@ -1,12 +1,9 @@
-var app =getApp();
-var show = false
+var app =getApp()
 Page({
   data:{
     "text":"邀请好友规则"
   },
   change:function(e){
-      // show = !show;
-      console.log(show);
       var id = e.currentTarget.id , data = {};
       data[id + 'Show'] = !this.data[id + 'Show'];
       this.setData(data);
