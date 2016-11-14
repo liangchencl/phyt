1b15782f05963da2c952bfec8e984cd3dc9d075f
凉城 <627150799@qq.com>
2016-11-14 16:18:36 +0800

    06

--- pages/tz/project/project.js
+++ pages/tz/project/project.js
@@ -1,6 +1,36 @@
 var app =getApp()
 Page({
   data:{
-    "text":"投资详情"
+    "text":"投资详情",
+    tab0Show:true
+  },
+  change:function(e){
+      var type = [
+          "tab0","tab1","tab2"
+      ];
+
+      var data = {};
+      var id = e.currentTarget.id;
+      //   console.log(id)
+      for(var i=0,len = type.length;i<len;i++){
+          data[type[i]+"Show"] = false ;
+      };
+      
+      // data[id+"Show"] = !this.data[id+"Show"];
+      // 点击自身的时候防止显示隐藏
+      if(data[id+"Show"]==false){
+        data[id+"Show"] = true;
+      }else{
+        data[id+"Show"] = !this.data[id+"Show"];
+      }
+      this.setData(data);
+  },
+  ShowHide:function(e){
+    var id = e.currentTarget.id;
+    var data = {};
+    data[id + "Show" ] = !this.data[id + "Show" ];
+    this.setData(data)
+    console.log(data)
+
   }
 })
