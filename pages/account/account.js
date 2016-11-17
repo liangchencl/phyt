f2e3a5c44866ef370f3b83c593a10a041073d31f
凉城 <627150799@qq.com>
2016-11-17 10:36:56 +0800

    07

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,6 +1,16 @@
 var app =getApp()
 Page({
   data:{
-    "text":"账户"
+    "text":"账户",
+    userInfo: {}
+  },
+  onLoad:function(){
+    var that = this;
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   }
 })
