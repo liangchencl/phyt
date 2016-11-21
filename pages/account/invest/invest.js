5d6ee0d20afed5d08bcd43d7b84b11ecae5a3ef7
凉城 <627150799@qq.com>
2016-11-21 16:56:33 +0800

    10

--- pages/account/invest/invest.js
+++ pages/account/invest/invest.js
@@ -2,7 +2,8 @@ var app =getApp()
 Page({
   data:{
     "text":"投资记录",
-    investList:[]
+    investList:[],
+    scrollTop : 0
   },
   onLoad:function(e){
     var that = this;
@@ -21,9 +22,27 @@ Page({
         //     investList:res.data.rows,
         //   })  
       }
-    })
+    }),
+    wx.getSystemInfo({
+        success:function(res){
+            console.info(res.windowHeight);
+            that.setData({
+                scrollHeight:res.windowHeight
+            });
+        }
+    });
   },
   change:function(){
     console.log(123)
-  }
+  },
+  bindDownLoad:function(){
+    console.log("滑动到了底部了")
+  },
+  scroll:function(event){
+    //   该方法绑定了页面滚动时的事件，我这里记录了当前的position.y的值,为了请求数据之后把页面定位到这里来。
+    console.log("开始滑动了")
+     this.setData({
+         scrollTop : event.detail.scrollTop
+     });
+  },
 })
