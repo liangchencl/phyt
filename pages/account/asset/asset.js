cdc80ff4fb020cefa0c78fc1b1c3b144a4b70ca5
凉城 <627150799@qq.com>
2016-11-22 16:30:44 +0800

    11

--- pages/account/asset/asset.js
+++ pages/account/asset/asset.js
@@ -19,22 +19,7 @@ Page({
 
     // 使用 wx.createContext 获取绘图上下文 context
 
-    var canvas1Id = "firstCanvas";
-    var canvas2Id = "secondCanvas";
-
-
     var context = wx.createContext();
-
-    // 调用 wx.drawCanvas，通过 canvasId 指定在哪张画布上绘制，通过 actions 指定绘制行为
-    // wx.drawCanvas({
-    //   canvasId: 'firstCanvas',
-    //   actions: context.getActions() // 获取绘图动作数组
-    // })
-
-    [canvas1Id, canvas2Id].forEach(function (id) {
-      context.clearActions()
-      // 在context上调用方法
-
       context.setLineWidth(15) // 线的宽度
 			context.beginPath();
       context.setStrokeStyle("#ff0000")
@@ -61,12 +46,35 @@ Page({
 			context.arc(75,100,60,2*Math.PI*(0.09+0.08+0.6+0.03),2*Math.PI*0.2);
 			context.stroke();
 
-
       wx.drawCanvas({
-        canvasId: id,
+        canvasId: "firstCanvas",
         actions: context.getActions()
       })
-    })
+
+      
+// 第二个canvas
+
+      context.setLineWidth(15) // 线的宽度
+			context.beginPath();
+      context.setStrokeStyle("#ff0000")
+			context.arc(75,100,60,0,2*Math.PI*0.4);
+      context.stroke();
+
+      context.beginPath();
+      context.setStrokeStyle("#ffff00")
+			context.arc(75,100,60,2*Math.PI*0.4,2*Math.PI*0.08);
+			context.stroke();
+
+      context.beginPath();
+      context.setStrokeStyle("#ff00ff")
+			context.arc(75,100,60,2*Math.PI*(0.4+0.08),2*Math.PI*0.3);
+			context.stroke();
+
+      wx.drawCanvas({
+        canvasId: "secondCanvas",
+        actions: context.getActions()
+      })
+
   }
 
 })
