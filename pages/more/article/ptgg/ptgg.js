5fddfc1281280d953a040eb9f34e6ba56b5a7668
凉城 <627150799@qq.com>
2017-01-10 10:48:54 +0800

    share

--- pages/more/article/ptgg/ptgg.js
+++ pages/more/article/ptgg/ptgg.js
@@ -2,5 +2,12 @@ var app =getApp()
 Page({
   data:{
     
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '普汇云准备金开放查询的公告',
+      desc: '普汇云准备金开放查询的公告普汇云准备金开放查询的公告',
+      path: '/pages/more/article/ptgg/ptgg'
+    }
   }
 })
